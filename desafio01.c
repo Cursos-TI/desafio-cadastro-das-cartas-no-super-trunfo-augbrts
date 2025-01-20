e7dbd098672ff3d7c7d720142c29eed90852efa3
augbrts <202501073808@alunos.estacio.br>
2025-01-20 19:17:44 -0300

    desafio01.c

--- desafio01.c
+++ desafio01.c
@@ -6,8 +6,10 @@ int main() {
     int populacao; //população e area coloquei como numeros inteiros.
     int area; // até tentei area como float mas percebi que não faz sentido. 
     float pib; //pib como float para ser um pouco mais detalhado.
-    int numero; 
-    
+    int numero;
+    float densidade;
+    float pibpc;
+
     printf("Qual o codigo da cidade? ");
     scanf("%s", &codigo);
     printf("O codigo da cidade e:  %s\n", codigo);
@@ -20,13 +22,20 @@ int main() {
     printf("Qual a area? ");
     scanf("%d", &area);
     printf("A area e: %dkm²\n", area);
-    printf("Qual o pib? ");
+    printf("Digite o PIB em bilhoes: (ex; 183.4) "); //Mudei o enunciado para ser mais claro.
     scanf("%f", &pib);
-    printf("O pib e: %f bi\n", pib);
+    pib = pib * 1e9; // Eu estava tendo erro pela forma que estava inserindo PIB. Essa linha multiplica por 1bi.
+    printf("O pib e: %.2f bi\n", pib);
     printf("Ha quantos pontos turisticos? ");
     scanf("%d", &numero);
     printf("Ha %d pontos turisticos\n", numero);
-    printf("Dados inseridos com sucesso!\n");
+
+    densidade = populacao / area;
+    pibpc = pib / populacao; 
+
+    printf("O pib per capta e de: %.2f\n", pibpc);
+    printf("A densidade populacional e de %.2f pessoas por km²\n", densidade);
+    printf("Carta registrada!\n");
 
     return 0;
 }
